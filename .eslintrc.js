43246eddbcbc783ee4911d0ca2429d4f0f7e9eb5
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-04-12 15:40:06 +0530

    :rainbow: added prettier + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ["plugin:vue/essential", "airbnb-base", "prettier", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module"
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "comma-dangle": "off",
+    quotes: ["error", "double"],
+    "prettier/prettier": "error"
+  }
+};
